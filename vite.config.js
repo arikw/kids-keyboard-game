580da4f17366b6163913c2117ee41a662b0065c6
arikw <7900646+arikw@users.noreply.github.com>
2022-07-07 08:42:31 +0300

    export the vue static sfc to an external project

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
+import staticVuePlugin from 'vite-plugin-vue-static-sfc';
+
 export default {
   plugins: [
-    {
-      name: 'vue-as-static',
-      apply: 'serve',
-      enforce: 'pre',
-      resolveId(source, importer) {
-        if (source.endsWith('.vue')) {
-          return '\ufeff' + source;
-        }
-      },
-      // HMR
-      handleHotUpdate({ file, server }) {
-        if (file.endsWith('.vue')) {
-          console.log((new Date()).toLocaleTimeString(), '[Static Vue] change detected');
-          server.ws.send({ type: 'full-reload', path: '*' });
-        }
-      }
-    }
+    staticVuePlugin()
   ]
 };
